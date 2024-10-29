50de2045eb4485d60d1deea089489169fc6233e0
muneo1008 <ansj011008@gmail.com>
2024-10-29 13:11:40 +0900

    Initial commit

---
+++ cody/src/main/java/com/muneo/cody/CodyApplication.java
@@ -0,0 +1,13 @@
+package com.muneo.cody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodyApplication.class, args);
+	}
+
+}
---
+++ cody/src/test/java/com/muneo/cody/CodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muneo.cody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
