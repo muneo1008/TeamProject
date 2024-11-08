f299c765b88f0f80dc7ca2e03fe3a0d8e447c445
muneo1008 <ansj011008@gmail.com>
2024-11-08 17:19:18 +0900

    appbar 설정 변경 및 수정

--- client/src/components/TopAppBar.jsx
+++ client/src/components/TopAppBar.jsx
@@ -4,25 +4,22 @@ import {useNavigate} from "react-router-dom";
 
 const TopAppBar = () => {
     const navigate = useNavigate();
-    const handleBackClick = () =>{
-        navigate(-1);
-    }
     return (
         <>
-            <Toolbar sx={{maxWidth:'600px',width:'100%', backgroundColor:'#C299FA',
+            <Toolbar sx={{maxWidth:'600px',width:'100%', backgroundColor:'#ffffff',
                 '@media (max-width: 768px)': {
                     maxWidth: '100%',
                 },}}>
-                <IconButton
-                    edge="start"
-                    color="inherit"
-                    onClick={handleBackClick}
-                    sx={{ mr: 2 }}
-                >
-                    <ArrowBackIcon />
-                </IconButton>
-                <Typography variant="h6">
-                    코디On
+                {/*<IconButton*/}
+                {/*    edge="start"*/}
+                {/*    color="inherit"*/}
+                {/*    onClick={handleBackClick}*/}
+                {/*    sx={{ mr: 2 }}*/}
+                {/*>*/}
+                {/*    <ArrowBackIcon />*/}
+                {/*</IconButton>*/}
+                <Typography variant="h5">
+                    Cody_on
                 </Typography>
             </Toolbar>
         </>
