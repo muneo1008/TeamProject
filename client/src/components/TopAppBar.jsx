7ffa01da371d988c90d16ae5ea458a48030e5cce
muneo1008 <ansj011008@gmail.com>
2024-11-13 10:22:40 +0900

    앱바가 다른 페이지에 가리던 점 수정

--- client/src/components/TopAppBar.jsx
+++ client/src/components/TopAppBar.jsx
@@ -1,23 +1,20 @@
-import {IconButton, Toolbar, Typography} from "@mui/material";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import {useNavigate} from "react-router-dom";
+import { Toolbar, Typography} from "@mui/material";
 
 const TopAppBar = () => {
-    const navigate = useNavigate();
     return (
         <>
-            <Toolbar sx={{maxWidth:'600px',width:'100%', backgroundColor:'#ffffff',
+            <Toolbar sx={{maxWidth:'600px',
+                width:'552px',
+                backgroundColor:'#ffffff',
+                position: 'fixed',
+                boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.1)',
+                zIndex:1100,
                 '@media (max-width: 768px)': {
                     maxWidth: '100%',
+                    width: '100%',
+
+                    paddingRight: '10px',
                 },}}>
-                {/*<IconButton*/}
-                {/*    edge="start"*/}
-                {/*    color="inherit"*/}
-                {/*    onClick={handleBackClick}*/}
-                {/*    sx={{ mr: 2 }}*/}
-                {/*>*/}
-                {/*    <ArrowBackIcon />*/}
-                {/*</IconButton>*/}
                 <Typography variant="h5">
                     Cody_on
                 </Typography>
