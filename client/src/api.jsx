162e718f0f36c9d77bda0d81c2f1231649591857
muneo1008 <ansj011008@gmail.com>
2024-11-26 10:31:53 +0900

    api 추가

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -147,3 +147,128 @@ export const getCodyImg = async (category,item,gender)=>{
         throw error;
     }
 }
+
+export const getSnapById = async (id)=>{
+    try{
+        const response = await api.get(`/snaps/${id}`)
+        return response.data;
+    }catch (err){
+        console.log('스냅 디테일 불러오기 실패',err);
+        throw err;
+    }
+}
+
+export const getSnapCommentById = async (id)=>{
+    try {
+        const response = await api.get(`/snaps/${id}/comments`);
+        return response.data;
+    }catch (err){
+        console.log('스냅 댓글 불러오기 실패',err);
+        throw err;
+    }
+}
+
+export const getSnapLikedBySnapId = async (id)=>{
+    try {
+        const response = await api.get(`/snaps/${id}/liked`);
+        return response.data;
+    }catch (err){
+        console.log('좋아요 불러오기 실패');
+        throw err;
+    }
+}
+
+export const getSnapList = async ()=>{
+    try {
+        const response = await api.get('/snaps');
+        return response.data;
+    }catch (err){
+        console.log('스냅 리스트 불러오기 실패');
+        throw err;
+    }
+}
+
+export const likeTrue = async (SnapId)=>{
+    try {
+        const response = await api.post(`/snaps/${SnapId}/like`);
+        return response.data;
+    }catch (err){
+        console.log('좋아요 실패');
+        throw err;
+    }
+}
+
+export const likeFalse = async (SnapId)=>{
+    try {
+        const response = await api.delete(`/snaps/${SnapId}/like`);
+        return response.data;
+    }catch (err){
+        console.log('좋아요 취소 실패');
+        throw err;
+    }
+}
+
+export const mySnaps = async ()=>{
+    try {
+        const response = await api.get('/snaps/my-snaps');
+        console.log('마이 스냅 불러오기 성공');
+        return response.data;
+    }catch (err){
+        console.log('마이 스냅 불러오기 실패');
+        throw err;
+    }
+}
+
+export const likedSnaps = async ()=>{
+    try {
+        const response = await api.get('/snaps/liked-snaps');
+        console.log('좋아요 스냅 불러오기 성공');
+        return response.data;
+    }catch (err){
+        console.log('좋아요 스냅 불러오기 실패');
+        throw err;
+    }
+}
+
+export const savePersonalColor = async (personalColor)=>{
+    try{
+        const response = await api.post('/member/personalColor',{personalColor:personalColor});
+        console.log('퍼컬 저장완료');
+        return response.data;
+    }catch (error){
+        console.log(error);
+        throw error;
+    }
+}
+
+export const OtherInfo = async (memberId)=>{
+    try {
+        const response = await api.get(`/member/${memberId}`);
+        console.log("타 유저 정보: ",response.data);
+        return response.data;
+    }catch (err){
+        console.log(err);
+        throw err;
+    }
+}
+
+export const OtherSnapInfo = async (memberId)=>{
+    try {
+        const response = await api.get(`/snaps/member/${memberId}`);
+        console.log("타 유저 스냅 정보: ",response.data);
+        return response.data;
+    }catch (err){
+        console.log(err);
+        throw err;
+    }
+}
+export const OtherLikedSnapInfo = async (memberId)=>{
+    try {
+        const response = await api.get(`/snaps/member/${memberId}/liked`);
+        console.log("타 유저 좋아요 스냅 정보: ",response.data);
+        return response.data;
+    }catch (err){
+        console.log(err);
+        throw err;
+    }
+}
