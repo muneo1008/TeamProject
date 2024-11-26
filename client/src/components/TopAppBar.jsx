b0edf5161cc8e784d4ac74b5d2e133eed6376b99
muneo1008 <ansj011008@gmail.com>
2024-11-26 10:27:45 +0900

    /snap에서 안나오게 변경

--- client/src/components/TopAppBar.jsx
+++ client/src/components/TopAppBar.jsx
@@ -1,25 +1,41 @@
-import { Toolbar, Typography} from "@mui/material";
-
+import {Box, Toolbar, Typography} from "@mui/material";
+import { useLocation } from "react-router-dom";
+import Logo from '../assets/logo.png'
 const TopAppBar = () => {
+    const location = useLocation();
+
+    // '/snap'으로 정확히 일치하는 경우에만 앱바 숨김
+    if (location.pathname === "/snap") {
+        return null;
+    }
+
     return (
-        <>
-            <Toolbar sx={{maxWidth:'600px',
-                width:'552px',
-                backgroundColor:'#ffffff',
+        <Toolbar
+            sx={{
+                maxWidth: '600px',
+                width: '552px',
+                backgroundColor: '#ffffff',
                 position: 'static',
                 boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.1)',
-                zIndex:1100,
+                zIndex: 1100,
                 '@media (max-width: 768px)': {
                     maxWidth: '100%',
                     width: '100%',
                     paddingRight: '10px',
-                },}}>
-                <Typography variant="h5">
-                    Cody_on
-                </Typography>
-            </Toolbar>
-        </>
-
+                },
+            }}
+        >
+            <Box
+                component="img"
+                src={Logo}
+                alt="Logo"
+                sx={{
+                    height: 40,
+                    width: 'auto',
+                }}
+            />
+        </Toolbar>
     );
 };
+
 export default TopAppBar;
